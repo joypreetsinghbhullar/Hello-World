b8cef9150b465790b0e89b8dfabb9197fcb17876
Time <jo-jojoy@outlook.com>
2024-11-25 19:49:29 +1030

    Update index.py

--- index.py
+++ index.py
@@ -8,3 +8,21 @@ rating = 4.9
 name = "blob"
 is_publish = True 
 print (price) # Shows what the price of the thing is 
+
+# John Smith Hospital
+name = "John Smith"
+age = 20 
+existing_patient = False 
+print (name)
+print (age)
+print (existing_patient)
+
+# taking in inputs
+input_name = input ("What is your name? ")
+print ("Hi " + input_name)
+
+# Color and What do they like and should say "USERNAME likes BLUE"
+
+user_input_name = input ("What is your name? ")
+favorite_color = input ("What is your favorite color? ")
+print (user_input_name + " likes " + favorite_color)

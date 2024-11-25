fdddab44a25bd9bf07d28259a7acf10b52c8a94d
Time <jo-jojoy@outlook.com>
2024-11-25 19:48:39 +1030

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
 print("Hello World") 
+print ("It work" * 2)
+
+# This is a variable that sets the price 
+price = 10 
+price = 20
+rating = 4.9
+name = "blob"
+is_publish = True 
+print (price) # Shows what the price of the thing is 
